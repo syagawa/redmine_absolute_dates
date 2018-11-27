ac4bacbaa43b29e6c9394b603f94b71f7961acee
syagawa <yagawa@qzc.co.jp>
2018-11-27 17:06:46 +0900

    Update init.rb

--- init.rb
+++ init.rb
@@ -7,11 +7,10 @@ end
 
 Rails.application.config.i18n.load_path += Dir["#{File.dirname(__FILE__)}/config/locales/*.{rb,yml}"]
 
-Redmine::Plugin.register :redmine_absolute_dates do
-  name 'Redmine Absolute Dates plugin'
-  author 'suer'
-  description 'Display absolute create or update dates '
-  version '0.0.3'
-  url 'https://github.com/suer/redmine_absolute_dates'
-  author_url 'http://d.hatena.ne.jp/suer'
+Redmine::Plugin.register :redmine_absolute_datetime do
+  name 'Redmine Absolute Dates & Time plugin'
+  author 'syagawa'
+  description 'Display absolute create or update dates & times'
+  version '0.0.4'
+  url 'https://github.com/syagawa/redmine_absolute_datetime'
 end
